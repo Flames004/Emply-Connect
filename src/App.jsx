7942b9378726a66ac0095896a98df64911b016d4
Deepak Shukla <flamesofwisdom004@gmail.com>
2024-11-17 00:34:33 +0530

    implementing context functionality in login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 const App = () => {
+  const [user, setuser] = useState(null)
 
-  useEffect(()=>{
-    getLocalStorage()
-  })
+  const handleLogin = (email,password)=>{
+    if(email=='admin@me.com' && password=='123'){
+      setuser('admin')
+    }
+    else if(email=='user@me.com' && password=='123') {
+      setuser('employee')
+    }
+    else{
+      alert('Invalid credentials!')
+    }
+  }
 
   return (
     <>
-    {/* <Login /> */}
-    {/* <EmployeeDashboard /> */}
-    {/* <AdminDashboard /> */}
+    {!user ? <Login handleLogin={handleLogin} /> : ''}
+    {user== 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   )
 }
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 
-const Login = () => {
+const Login = ({handleLogin}) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const submitHandler = (e)=>{
         e.preventDefault()
+        handleLogin(email,password)
         setEmail('')
         setPassword('')
-        console.log("Form Submitted!")
+        // console.log("Form Submitted!")
     }
 
   return (
